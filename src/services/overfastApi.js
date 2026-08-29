0b9f8b62033b71ec6259751236c6e80482b9df4b
Lucas <llucaslleal2006@gmail.com>
2026-08-29 17:38:30 -0300

    chore: criação da estrutura inicial de pastas e arquivos.

---
+++ src/services/overfastApi.js
@@ -0,0 +1,44 @@
+const BASE_URL = 'https://overfast-api.tekrop.fr';
+
+export async function getHeroes(signal){
+    try{
+    const response = await fetch(`${BASE_URL}/heroes`,{signal,});
+
+    if(!response.ok){
+        throw new Error(`erro ao buscar os heróis na Api: ${response.status} - ${response.statusText}`);
+    }
+     
+    return await response.json();
+
+    }catch(error){
+        if(error.name === 'AbortError'){
+            console.log("Requisição cancelada.");
+
+            return;
+        }
+        console.error(error);
+
+        console.log("Falha ao buscar lista dos heróis na Api.",error);
+
+        throw new Error("Não foi possível carregar os heróis.",{cause: error});
+    }
+}
+
+export async function getHeroById(heroId, signal){
+    try{
+    const response = await fetch(`${BASE_URL}/heroes/${heroId}`,{signal,});
+
+    if(!response.ok){
+        throw new Error(`erro na Api(getHeroById): ${response.status}`);
+    }
+    return await response.json();
+
+    }catch(error){
+        if(error.name === 'AbortError') return;
+
+        console.error(`Falha ao buscar detalhe do herói ${heroId}:`,error);
+
+        throw new Error("Não foi possível carregar as informações do herói.",{cause: error});
+        }
+    
+}
